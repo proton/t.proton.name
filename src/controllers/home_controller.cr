016b1399c02b03faf00be1a557035c1030262926
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-20 15:53:38 -0400

    basic request to yandex disk

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,5 +1,15 @@
+require "crest"
+
 class HomeController < ApplicationController
   def index
+    yandex_disk_token = ENV["YANDEX_DISK_TOKEN"]
+
+    photos = Crest.get(
+      "https://cloud-api.yandex.net/v1/disk/resources?path=t.proton.name-content%2Fphoto",
+      params: {:lang => "en"},
+      headers: {"Accept" => "application/json", "Authorization" => "OAuth #{yandex_disk_token}"},
+    )
+
     render("index.slang")
   end
 end
