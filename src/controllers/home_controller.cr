e6ec38fb8874fbb7071098880424a140b3296f38
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-20 21:45:07 -0400

    proper file path pass

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -3,14 +3,15 @@ require "crest"
 class HomeController < ApplicationController
   def index
     yandex_disk_token = ENV["YANDEX_DISK_TOKEN"]
+    yandex_disk_video_path = ENV["YANDEX_DISK_VIDEO_PATH"]
 
-    videos = load_videos(yandex_disk_token)
+    videos = load_videos(yandex_disk_token, yandex_disk_video_path)
     medias = videos
     render("index.slang")
   end
   
-  def load_videos(yandex_disk_token)
-    load_yandex_disk_files("video", yandex_disk_token).map do |x|
+  def load_videos(token, path)
+    load_yandex_disk_files(token, path).map do |x|
       name = x["name"].as_s
       {
         "type" => "video",
@@ -24,10 +25,10 @@ class HomeController < ApplicationController
     end
   end
 
-  def load_yandex_disk_files(path, token)
+  def load_yandex_disk_files(token, path)
     json = Crest.get(
-      "https://cloud-api.yandex.net/v1/disk/resources?path=t.proton.name-content%2F#{path}",
-      params: {:lang => "en"},
+      "https://cloud-api.yandex.net/v1/disk/resources",
+      params: {"path" => path},
       headers: {"Accept" => "application/json", "Authorization" => "OAuth #{token}"},
     ).body
     JSON.parse(json)["_embedded"]["items"].as_a
