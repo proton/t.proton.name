a23910961f1e855e1329b752e674853f691ac917
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-21 20:24:23 -0400

    restored template

--- server.js
+++ server.js
@@ -10,7 +10,88 @@ app.listen(3000, _ => {
 })
 
 app.get('/', (_req, res) => {
-  // load medias
+  const yandexDiskToken = process.env.yandexDiskTOKEN
+  const yandexDiskVideoPath = process.env.yandexDiskVIDEO_PATH
+  const yandexDiskAudioPath = process.env.yandexDiskAUDIO_PATH
+  const yandexDiskPhotoFullPath = process.env.yandexDiskPHOTO_FULL_PATH
+  const yandexDiskPhotoPreviewPath = process.env.yandexDiskPHOTO_PREVIEW_PATH
 
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  const videos = loadVideos(yandexDiskToken, yandexDiskVideoPath)
+  const audios = loadAudios(yandexDiskToken, yandexDiskAudioPath)
+  const photos = loadPhotos(yandexDiskToken, yandexDiskPhotoFullPath, yandexDiskPhotoPreviewPath)
+
+  const medias = videos.concat(audios, photos)
+  // medias = medias.sort_by { |m| m["date"].to_s }.reverse
+
+  // years = medias.map { |m| m["year"] }.uniq
+  const years = [1988, 2020]
+  
+
+  res.render('index', { medias: medias, years: years })
 })
+
+const loadVideos = () => { return [] }
+const loadAudios = () => { return [] }
+const loadPhotos = () => { return [] }
+
+/*
+  def load_videos(token, path)
+    load_yandexDiskfiles(token, path).map do |x|
+      name = x["name"].as_s
+      {
+        "type" => "video",
+        "url" => x["file"],
+        "name" => name,
+        "preview" => x["preview"],
+        "date" => name[0...10],
+        "year" => name[0...4],
+        "is_hidden" => name.includes?("hidden")
+      }
+    end
+  end
+  
+  def load_audios(token, path)
+    load_yandexDiskfiles(token, path).map do |x|
+      name = x["name"].as_s
+      {
+        "type" => "audio",
+        "url" => x["file"],
+        "name" => name,
+        # "preview" => x["preview"],
+        "date" => name[0...10],
+        "year" => name[0...4],
+        "is_hidden" => name.includes?("hidden")
+      }
+    end
+  end
+  
+  def load_photos(token, originals_path, previews_path)
+    originals = load_yandexDiskfiles(token, originals_path).index_by { |x| x["name"].as_s }
+    previews = load_yandexDiskfiles(token, previews_path)
+
+    previews.map do |x|
+      name = x["name"].as_s
+      url = originals[name]["file"]
+      {
+        "type" => "photo",
+        "url" => url,
+        "name" => name,
+        "preview" => x["file"],
+        "date" => name[0...10],
+        "year" => name[0...4],
+        "is_hidden" => name.includes?("hidden")
+      }
+    end
+  end
+
+  def load_yandexDiskfiles(token, path)
+    json = Crest.get(
+      "https://cloud-api.yandex.net/v1/disk/resources",
+      params: {"path" => path, "limit" => 1000 },
+      headers: {"Accept" => "application/json", "Authorization" => "OAuth #{token}"},
+    ).body
+    JSON.parse(json)["_embedded"]["items"].as_a
+  end
+end
+
+*/
