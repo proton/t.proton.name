5b5dad2344891d4f52c09a75ab7a3119c3f804f0
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-21 21:27:04 -0400

    implemented all load methods

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const moment = require('moment')
 
+const axios = require('axios').default
+
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
@@ -9,18 +11,18 @@ app.listen(3000, _ => {
   console.log(`listening on ${3000}`)
 })
 
-app.get('/', (_req, res) => {
-  const yandexDiskToken = process.env.yandexDiskTOKEN
-  const yandexDiskVideoPath = process.env.yandexDiskVIDEO_PATH
-  const yandexDiskAudioPath = process.env.yandexDiskAUDIO_PATH
-  const yandexDiskPhotoFullPath = process.env.yandexDiskPHOTO_FULL_PATH
-  const yandexDiskPhotoPreviewPath = process.env.yandexDiskPHOTO_PREVIEW_PATH
+app.get('/', async (_req, res) => {
+  const yandexDiskToken = process.env.YANDEX_DISK_TOKEN
+  const yandexDiskVideoPath = process.env.YANDEX_DISK_VIDEO_PATH
+  const yandexDiskAudioPath = process.env.YANDEX_DISK_AUDIO_PATH
+  const yandexDiskPhotoFullPath = process.env.YANDEX_DISK_PHOTO_PREVIEW_PATH
+  const yandexDiskPhotoPreviewPath = process.env.YANDEX_DISK_PHOTO_FULL_PATH
 
-  const videos = loadVideos(yandexDiskToken, yandexDiskVideoPath)
-  const audios = loadAudios(yandexDiskToken, yandexDiskAudioPath)
-  const photos = loadPhotos(yandexDiskToken, yandexDiskPhotoFullPath, yandexDiskPhotoPreviewPath)
+  const videos = await loadVideos(yandexDiskToken, yandexDiskVideoPath)
+  const audios = await loadAudios(yandexDiskToken, yandexDiskAudioPath)
+  const photos = await loadPhotos(yandexDiskToken, yandexDiskPhotoFullPath, yandexDiskPhotoPreviewPath)
 
-  const medias = videos.concat(audios, photos)
+  const medias = [].concat(videos, audios, photos)
   // medias = medias.sort_by { |m| m["date"].to_s }.reverse
 
   // years = medias.map { |m| m["year"] }.uniq
@@ -30,68 +32,61 @@ app.get('/', (_req, res) => {
   res.render('index', { medias: medias, years: years })
 })
 
-const loadVideos = () => { return [] }
-const loadAudios = () => { return [] }
-const loadPhotos = () => { return [] }
+const loadYandexDiskFiles = async(token, path) => {
+  const response = await axios.get('https://cloud-api.yandex.net/v1/disk/resources', {
+    params: { path: path, limit: 1000 },
+    headers: {'Accept': 'application/json', 'Authorization': `OAuth ${token}`}
+  })
 
-/*
-  def load_videos(token, path)
-    load_yandexDiskfiles(token, path).map do |x|
-      name = x["name"].as_s
-      {
-        "type" => "video",
-        "url" => x["file"],
-        "name" => name,
-        "preview" => x["preview"],
-        "date" => name[0...10],
-        "year" => name[0...4],
-        "is_hidden" => name.includes?("hidden")
-      }
-    end
-  end
-  
-  def load_audios(token, path)
-    load_yandexDiskfiles(token, path).map do |x|
-      name = x["name"].as_s
-      {
-        "type" => "audio",
-        "url" => x["file"],
-        "name" => name,
-        # "preview" => x["preview"],
-        "date" => name[0...10],
-        "year" => name[0...4],
-        "is_hidden" => name.includes?("hidden")
-      }
-    end
-  end
-  
-  def load_photos(token, originals_path, previews_path)
-    originals = load_yandexDiskfiles(token, originals_path).index_by { |x| x["name"].as_s }
-    previews = load_yandexDiskfiles(token, previews_path)
+  return response.data._embedded.items
+}
 
-    previews.map do |x|
-      name = x["name"].as_s
-      url = originals[name]["file"]
-      {
-        "type" => "photo",
-        "url" => url,
-        "name" => name,
-        "preview" => x["file"],
-        "date" => name[0...10],
-        "year" => name[0...4],
-        "is_hidden" => name.includes?("hidden")
-      }
-    end
-  end
+const loadVideos = async (token, path) => {
+  const json = await loadYandexDiskFiles(token, path);
+  return json.map(x => {
+    return {
+      type: "video",
+      url: x.file,
+      name: x.name,
+      preview: x.preview,
+      date: x.name.slice(0, 10),
+      year: +x.name.slice(0, 4),
+      isHidden: x.name.includes("hidden")
+    }
+  })
+}
 
-  def load_yandexDiskfiles(token, path)
-    json = Crest.get(
-      "https://cloud-api.yandex.net/v1/disk/resources",
-      params: {"path" => path, "limit" => 1000 },
-      headers: {"Accept" => "application/json", "Authorization" => "OAuth #{token}"},
-    ).body
-    JSON.parse(json)["_embedded"]["items"].as_a
-  end
-end
+const loadAudios = async (token, path) => {
+  const json = await loadYandexDiskFiles(token, path);
+  return json.map(x => {
+    return {
+      type: "audio",
+      url: x.file,
+      name: x.name,
+      date: x.name.slice(0, 10),
+      year: +x.name.slice(0, 4),
+      isHidden: x.name.includes("hidden")
+    }
+  })
+}
 
-*/
+const loadPhotos = async (token, originalsPath, previewsPath) => {
+  const jsonOriginals = await loadYandexDiskFiles(token, originalsPath)
+  const jsonPreviews = await loadYandexDiskFiles(token, previewsPath)
+
+  const originals = Object.assign({}, ...jsonOriginals.map((x) => ({[x.name]: x})));
+
+  return jsonPreviews.map(x => {
+    const url = originals[x.name].file
+
+    return {
+      type: "photo",
+      url: url,
+      name: x.name,
+      preview: x.file,
+      date: x.name.slice(0, 10),
+      year: +x.name.slice(0, 4),
+      isHidden: x.name.includes("hidden")
+    }
+  })
+}
